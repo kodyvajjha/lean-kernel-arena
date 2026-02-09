c302e5abffca7da0c048b6d9e6994d0fa9d3f3d4
Joachim Breitner <mail@joachim-breitner.de>
2026-02-09 11:30:30 +0100

    initState

--- tutorial/Tutorial/Export.lean
+++ tutorial/Tutorial/Export.lean
@@ -4,10 +4,7 @@ open Lean
 def exportDeclsFromEnv (env : Lean.Environment) (constants : Array Name) : IO Unit := do
   initSearchPath (← findSysroot)
   M.run env do
-    modify (fun st => { st with
-      exportMData  := false
-      exportUnsafe := false
-    })
+    initState env
     dumpMetadata
     for c in constants do
       modify (fun st => { st with noMDataExprs := {} })
